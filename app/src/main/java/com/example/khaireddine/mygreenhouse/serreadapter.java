99d43396e8f8bc0a5f4f06e166e9324b43e4324b
firas salem <32462863+firasgh@users.noreply.github.com>
2018-04-16 18:57:08 +0200

    2eme commit

---
+++ app/src/main/java/com/example/khaireddine/mygreenhouse/serreadapter.java
@@ -0,0 +1,75 @@
+package com.example.khaireddine.mygreenhouse;
+
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class serreadapter extends BaseAdapter {
+    private Context context;
+    private final String[] mobileValues;
+
+    public serreadapter(Context context, List<String> mobileValues) {
+        this.context = context;
+        this.mobileValues = mobileValues.toArray(new String[0]);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+
+            gridView = new View(context);
+
+            // get layout from mobile.xml
+            gridView = inflater.inflate(R.layout.itemserre, null);
+
+            // set value into textview
+            TextView textView = (TextView) gridView
+                    .findViewById(R.id.grid_item_label);
+            textView.setText(mobileValues[position]);
+
+            // set image based on selected text
+            ImageView imageView = (ImageView) gridView
+                    .findViewById(R.id.grid_item_image);
+
+            String mobile = mobileValues[position];
+
+
+            imageView.setImageResource(R.drawable.greenhouse);
+
+
+        } else {
+            gridView = (View) convertView;
+        }
+
+        return gridView;
+    }
+
+    @Override
+    public int getCount() {
+        return mobileValues.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+}
